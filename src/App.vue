e39622717b7e863f8fbef17f03fc2aea9e4a673a
Lurge <glngwenya@gmail.com>
2021-07-04 21:42:59 +0300

    kimepop

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="the-app">
+    <TheCarousel msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheCarousel from './components/TheCarousel.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TheCarousel
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+html,
+body{
+  height: 100%;
+  font-family: "Roboto", san-serif;
+}
+
+#app{ 
+  height: 100%;
 }
 </style>
---
+++ src/components/TheCarousel.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="carousel">
+    <div class="carousel__item carousel__item--visible">
+      <img src="../assets/imgs/one.jpg" alt="" class="carousel__img">
+    </div>
+    <div class="carousel__item carousel__item--hidden">
+      <img src="../assets/imgs/two.jpg" alt="" class="carousel__img">
+    </div>
+    <div class="carousel__item carousel__item--hidden">
+      <img src="../assets/imgs/three.jpg" alt="" class="carousel__img">
+    </div>
+
+    <div class="carousel__actions">
+      <button class="carousel__btn carousel__btn--prev" @click.prevent="prev">Prev</button>
+      <button class="carousel__btn carousel__btn--next" @click.prevent="next">Next</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+    name: "the-carousel",
+
+    data() {
+      return {
+        slides: null,
+        totalSlides: null,
+        position: 0,
+
+      }
+    },
+
+    created() {
+      this.slides = document.getElementsByClassName('carousel__item')
+      console.log(document.getElementsByClassName('carousel__item'))
+      this.totalSlides = this.slides.length
+
+    },
+
+    methods: {
+
+      prev() {
+        console.log('previous')
+        console.log('this.totalSlides', this.totalSlides)
+        if (this.position === this.totalSlides - 1) {
+          this.position = 0
+        } else {
+          this.position--
+        }
+        this.updateSlidePosition()
+      },
+
+      next() {
+        console.log('next')
+        console.log('this.totalSlides', this.totalSlides)
+        if (this.position == 0) {
+          this.position = 0
+        } else {
+          this.position++
+        }
+        this.updateSlidePosition()
+        console.log('this.position', this.position)
+      },
+
+      updateSlidePosition() {
+        for (let slide of this.slides){
+          slide.classList.remove('carousel__item--visible')
+          slide.classList.add('carousel__item--hidden')
+          console.log('slide', slide)
+        }
+        this.slides[this.position].classList.add('carousel__item--visible')
+      }
+    },
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .carousel {
+    height: auto;
+    overflow: hidden;
+    max-width: 600px;
+    position: relative;
+
+  }
+
+  .carouse .carousel__item .carousel__item--hidden {
+    display: none;
+  }
+
+  .carouse .carousel__item .carousel__item--visible {
+    display: block;
+  }
+
+  .carousel__img {
+    height: auto;
+    width: 100%;
+    max-width: 600px;
+    object-fit: cover;
+    object-position: center;
+  }
+
+  .carousel__item--hidden {
+    display: none;
+  }
+
+  .carousel__item--visible {
+    display: block;
+  }
+
+  .carousel__actions {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .carousel__btn {
+    padding: 5px 10px;
+    border: 2px solid white;
+    background: transparent;
+    color: white;
+    font-weight: 700;
+    cursor: pointer;
+    outline: none;
+  }
+
+  .carousel__btn--prev {
+    margin-left: 16px;
+  }
+
+  .carousel__btn--next {
+    margin-right: 16px;
+  }
+</style>
